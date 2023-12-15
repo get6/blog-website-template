983a6fef7325055626cd2579aaf7dc16894227f9
get6 <ittae.com@gmail.com>
2023-12-15 16:03:11 +0900

    Change config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const { withContentlayer } = require('next-contentlayer')
 
+const prod = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, swcMinify: true }
+const nextConfig = { reactStrictMode: true, swcMinify: true, ...prod && { output: 'export' } }
 
 module.exports = withContentlayer(nextConfig)
